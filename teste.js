2acd3180be9e4b412a69dc7c580933657d948313
Joaozote <joao.miguel.ls@hotmail.com>
2024-11-18 00:13:49 -0300

    creating the hoover effect

--- teste.js
+++ teste.js
@@ -2,6 +2,35 @@ const gridContainer = document.querySelector("#grid-container");
 const gridButton = document.getElementById("btnCreate");
 gridButton.addEventListener("click", createGrid);
 
+// function createGrid() {
+//     gridContainer.innerHTML = '';
+//     const gridSize = parseInt(document.getElementById('input').value);
+//     if (!gridSize || isNaN(gridSize) || gridSize <= 0 || gridSize > 100) {
+//         alert("Please choose a number between 1 and 100");
+
+//     } else {
+
+//         console.log(gridSize)
+//         for (let i = 0; i < gridSize; i++) {
+//             let column = document.createElement("div");
+//             column.classList.add("column");
+//             for (let j = 0; j < gridSize; j++) {
+//                 let row = document.createElement('div');
+//                 row.classList.add("row")
+//                 const cellSize = 500 / gridSize;
+//                 row.style.width = cellSize + "px";
+//                 row.style.height = cellSize + "px";
+//                 row.classList.add('grid-cell');
+//                 column.appendChild(row);
+//             }
+//             gridContainer.appendChild(column);
+//         }
+//         addHoverEffect();
+//         gridContainer.style.width = gridContainer.clientHeight + 'px';
+
+//     }
+// }
+
 function createGrid() {
     gridContainer.innerHTML = '';
     const gridSize = parseInt(document.getElementById('input').value);
@@ -17,11 +46,11 @@ function createGrid() {
             cell.style.height = cellSize + "px";
             column.push(cell.outerHTML);
         }
-
+        
         for (let i = 0; i < gridSize; i++) {
             const columnContainer = document.createElement('div');
             columnContainer.classList.add('column');
-            columnContainer.innerHTML = column.join('');
+            columnContainer.innerHTML = column.join(''); 
             gridContainer.appendChild(columnContainer);
         }
         addHoverEffect();
@@ -39,7 +68,7 @@ function getRandomColor() {
 
 function addHoverEffect() {
     // Selecione todas as células (com a classe grid-cell)
-    const gridCells = document.querySelectorAll('.grid-cell');
+    const gridCells = document.querySelectorAll('.grid-cell');  
 
     gridCells.forEach(cell => {
         // Evento para quando o mouse entra na célula
